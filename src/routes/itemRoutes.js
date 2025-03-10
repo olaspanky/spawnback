63b70c32c792160cc1f79d702c63600010ab5909
Olakareem <olakareemomobolarinwa@gmail.com>
2025-03-10 13:12:37 +0100

    first commit

--- src/routes/itemRoutes.js
+++ src/routes/itemRoutes.js
@@ -1,11 +1,27 @@
-// routes/itemRoutes.js
 const express = require('express');
-const { createItem, getItems, getItem } = require('../controllers/itemController');
+const {
+  createItem,
+  getItems,
+  getItem,
+  updateItem,
+  deleteItem
+} = require('../controllers/itemController');
 const authMiddleware = require('../middlewares/authMiddleware');
 const router = express.Router();
 
+// Create a new item
 router.post('/items', authMiddleware, createItem);
+
+// Get all items
 router.get('/items', getItems);
+
+// Get a single item by ID
 router.get('/items/:id', getItem);
+
+// Update an item by ID
+router.put('/items/:id', authMiddleware, updateItem);
+
+// Delete an item by ID
+router.delete('/items/:id', authMiddleware, deleteItem);
 
 module.exports = router;
